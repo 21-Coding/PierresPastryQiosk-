1adb6c62831372c6e333f6a4823a1d6ee008e4ec
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-28 20:18:30 -0700

    update account controller.

--- PierresTreats/Controllers/AccountsController.cs
+++ PierresTreats/Controllers/AccountsController.cs
@@ -3,3 +3,66 @@ using Microsoft.AspNetCore.Identity;
 using Pierre.Models;
 using System.Threading.Tasks;
 using Pierre.ViewModels;
+
+namespace Pierre.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PierresTreatsContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresTreatsContext db)
+        {
+            _userManager = userManager;
+            _signInManager = _signInManager;
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            var user new ApplicationUser { UserName = model.Email};
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInMagager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
