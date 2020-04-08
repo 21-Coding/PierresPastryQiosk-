2e768d5ad86837c66cad84d9fc342d55d8b2738d
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-07 20:33:48 -0700

    add string[] args to line 10 of design time.

--- PierresTreats/Models/DesignTimeDbContextFactory.cs
+++ PierresTreats/Models/DesignTimeDbContextFactory.cs
@@ -7,18 +7,19 @@ namespace Pierre.Models
 {
   public class PierresTreatsContextFactory : IDesignTimeDbContextFactory<PierresTreatsContext>
   {
-    PierresTreatsContext IDesignTimeDbContextFactory<PierresTreatsContext>.CreateDbContext()
+    PierresTreatsContext IDesignTimeDbContextFactory<PierresTreatsContext>.CreateDbContext(string[] args)
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile("appsettings.json")
         .Build();
 
-      var builder = new DbContextOptionsBuilder<PierresTreatsContext>();
-      var connectionString = configuration.GetConnectionString("DefaultConnection");
+        var builder = new DbContextOptionsBuilder<PierresTreatsContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
 
-      builder.UseMySql(connectionString);
+        builder.UseMySql(connectionString);
 
+        return new PierresTreatsContext(builder.Options);
+      }
     }
   }
-}
