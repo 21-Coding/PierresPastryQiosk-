492733a2257ca16bf95d27ae8e9940bb90b05533
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-07 20:13:20 -0700

    add line of code that will link my appsettings file to my code base.

--- PierresTreats/Models/DesignTimeDbContextFactory.cs
+++ PierresTreats/Models/DesignTimeDbContextFactory.cs
@@ -11,6 +11,7 @@ namespace Pierre.Models
     {
       IConfigurationRoot configuration = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
     }
   }
 }
